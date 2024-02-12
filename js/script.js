670a2bf871126118a51b702f81054204907bf602
ZiRazok <45107612+ZiRazok@users.noreply.github.com>
2024-02-13 01:34:14 +0200

    Changes 5
    if modal window or menu is open then "body - overflow: hidden"

--- js/script.js
+++ js/script.js
@@ -5,11 +5,13 @@ const refs = {
 
     openMenuBtn: document.getElementById('openMenuBtn'),
     closeMenuBtn: document.getElementById('closeMenuBtn'),
-    menuWindow: document.getElementById('menuWindow')
+    menuWindow: document.getElementById('menuWindow'),
+
+    body: document.body
 };
 
-refs.openModalBtn.addEventListener('click', () => { refs.modalWindow.classList.toggle('is-open') });
-refs.closeModalBtn.addEventListener('click', () => { refs.modalWindow.classList.toggle('is-open') });
+refs.openModalBtn.addEventListener('click', () => { refs.modalWindow.classList.toggle('is-open'); refs.body.style.overflow = 'hidden' });
+refs.closeModalBtn.addEventListener('click', () => { refs.modalWindow.classList.toggle('is-open'); refs.body.style.overflow = 'auto' });
 
-refs.openMenuBtn.addEventListener('click', () => { refs.menuWindow.classList.toggle('is-open') });
-refs.closeMenuBtn.addEventListener('click', () => { refs.menuWindow.classList.toggle('is-open') });
+refs.openMenuBtn.addEventListener('click', () => { refs.menuWindow.classList.toggle('is-open'); refs.body.style.overflow = 'hidden' });
+refs.closeMenuBtn.addEventListener('click', () => { refs.menuWindow.classList.toggle('is-open'); refs.body.style.overflow = 'auto' });
